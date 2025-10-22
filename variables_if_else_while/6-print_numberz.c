71368811c674a387e82526f2ffefa22f1a86207c
Mathy974 <12327@holbertonstudents.com>
2025-10-22 08:48:03 +0000

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - imprime les chiffres de 0 à 9 sans utiliser de char
+ *
+ * Return: 0 (succès)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+putchar(i + 0); 
+putchar('\n');
+return (0);
+}
