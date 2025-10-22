2c298d7f5ea2c234ea6d7c69da59f0448ba6ccfb
Mathy974 <12327@holbertonstudents.com>
2025-10-22 08:11:56 +0000

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - imprime l'alphabet en minuscules sauf 'e' et 'q'
+ *
+ * Return: 0 (succès)
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+if (c != 'e' && c != 'q')
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
