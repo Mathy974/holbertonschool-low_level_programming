a49bc6ffe3674180113f4122214f93a0e0223ed7
Mathy974 <12327@holbertonstudents.com>
2025-10-23 11:17:59 +0000

    o

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -3,19 +3,18 @@
 #include "main.h"
 
 /**
- * main - prints _putchar, followed by a new line.
+ * print_alphabet - prints _putchar, followed by a new line.
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void)
 {
-char c = "a";
-while (c <= "z")
-{
-printf('%c', c)
-c++;
-}
-putchar('\n')
-putchar(" print_alphabet")
-return (0);
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
 }
