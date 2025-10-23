d2c728f3de5f50fa0681799d3ddc78c7123110ee
Mathy974 <12327@holbertonstudents.com>
2025-10-23 10:45:31 +0000

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - prints _putchar, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c = "a";
+while (c <= "z")
+{
+printf('%c', c)
+c++;
+}
+putchar('\n')
+putchar(" print_alphabet")
+return (0);
+}
